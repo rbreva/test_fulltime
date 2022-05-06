ff1f79e4d50cab91a362152ba4e1a92023105029
Mauricio Mendoza <rbreva@gmail.com>
2022-05-06 11:37:22 -0500

    Actualizacion con estilos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
+
+const token = "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"
+
 function createCard(repo){
     console.log('repo', repo)
     const card = document.createElement('article');
-  
+    const {
+        name
+    } = repo
+
+    const appContainer = document.getElementById('app')
+
+    card.innerHTML = name 
+
+    appContainer.appendChild(card)
 }
 
 fetch('https://api.github.com/users/rbreva/repos')
